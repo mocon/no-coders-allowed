78f9dc1dd69b3c14da38f94b394aa309e123c83f
Myles O'Connor <myles.oconnor@gmail.com>
2023-03-10 15:26:04 -0800

    Skip login screen, go directly to GitHub for auth

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -52,16 +52,6 @@ export const api = createTRPCNext<AppRouter>({
   ssr: false,
 })
 
-/**
- * Inference helper for inputs.
- *
- * @example type HelloInput = RouterInputs['example']['hello']
- */
 export type RouterInputs = inferRouterInputs<AppRouter>
 
-/**
- * Inference helper for outputs.
- *
- * @example type HelloOutput = RouterOutputs['example']['hello']
- */
 export type RouterOutputs = inferRouterOutputs<AppRouter>
